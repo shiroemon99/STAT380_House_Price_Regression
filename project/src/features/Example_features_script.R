28cb5f54c428f5a9a9b23d9159e615a2a4d5a530
shiroemon99 <shiroemongong@gmail.com>
2021-02-06 21:19:32 -0500

    raw data transformed to intermidiate data

--- project/src/features/Example_features_script.R
+++ project/src/features/Example_features_script.R
@@ -1,4 +1,15 @@
+# Package Preparation
 library(data.table)
 set.seed(77)
 
-DT <- fread('./project/volume/data/raw/Stat_380_train.csv')
+# Read raw data tables
+DT_train <- fread('./project/volume/data/raw/Stat_380_train.csv')
+DT_test <- fread('./project/volume/data/raw/Stat_380_test.csv')
+
+# Rewrite null values to zero
+DT_train[is.na(DT_train)] <- 0
+DT_test[is.na(DT_test)] <- 0
+
+# Export the model-ready data tables
+fwrite(DT_train, './project/volume/data/interim/Stat_380_train.csv')
+fwrite(DT_test, './project/volume/data/interim/Stat_380_test.csv')
